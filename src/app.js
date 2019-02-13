829cf4e3933a1d30bbd7a9c8843e28d2be7b8d0a
Leonardo Motta <leo@domboscoatacado.com.br>
2019-02-13 12:03:16 -0200

    Basic setup

---
+++ src/app.js
@@ -0,0 +1,48 @@
+const http = require('http');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const server = http.Server(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.status || 500;
+  const message = error.message;
+  res.status(status).json({ message: message, status: status });
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${
+      process.env.DB_PASSWORD
+    }@cluster0-sud5s.mongodb.net/${process.env.DB_NAME}`,
+    { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true }
+  )
+  .then(result => {
+    server.listen(process.env.PORT || 8080, () => {
+      console.log(`App listening on port ${process.env.PORT || 8080}`);
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
